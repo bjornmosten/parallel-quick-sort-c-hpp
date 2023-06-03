7eeebd952a35be2b3cf42fb4d1f14ad7907307bf
bjorn <you@example.com>
2023-06-03 18:51:46 +0200

    working

--- main.c
+++ main.c
@@ -16,12 +16,11 @@ typedef struct thread_data {
     int *data;
     // Pointer to the start of the data array, used for freeing the memory
     int *data_free;
-    int *__restrict new_data;
+    int *new_data;
     // Temporary data for swapping and moving
-    int *__restrict tmp_data;
-    int *merge;
     int data_n;
-    int merge_n;
+    int new_data_n;
+    int split_index;
     int tid;
 } thread_data_t;
 
@@ -49,9 +48,6 @@ void print_one_thread(thread_data_t *thread_data) {
         printf("%d ", thread_data->data[i]);
     }
     printf("\nThread %d merge: ", thread_data->tid);
-    for (int i = 0; i < thread_data->merge_n; i++) {
-        printf("%d ", thread_data->merge[i]);
-    }
     printf("\n");
 }
 
@@ -65,31 +61,89 @@ void print_array(thread_data_t **thread_data, int threads_n) {
     }
 }
 
-void move_values(thread_data_t *__restrict thread_0,
-                 thread_data_t *__restrict thread_1) {
-    int tmp_data_index = 0;
-    int data_index = 0;
-    int new_data_n = thread_1->merge_n + thread_0->data_n-1;
-    thread_0->new_data = (int *)malloc(sizeof(int) * (new_data_n));
-    #pragma omp barrier
-    for(int i = 0; i < new_data_n; i++) {
-        if(tmp_data_index <= thread_1->merge_n && thread_1->tmp_data[tmp_data_index] <= thread_0->data[data_index]) {
-            thread_0->new_data[i] = thread_1->tmp_data[tmp_data_index];
-            tmp_data_index++;
+int * merge_data(thread_data_t *__restrict thread_0,
+                thread_data_t *__restrict thread_1, bool is_lower) {
+    int thread_0_index;
+    int thread_1_index;
+    int * new_data;
+    int new_data_n;
+    int i = 0;
+    if(is_lower) {
+        thread_0_index = 0;
+        thread_1_index = 0;
+        new_data_n = thread_0->split_index + thread_1->split_index;
+        new_data = (int *)malloc(sizeof(int) * new_data_n);
+        while (thread_0_index < thread_0->split_index &&
+               thread_1_index < thread_1->split_index) {
+            if (thread_0->data[thread_0_index] <
+                thread_1->data[thread_1_index]) {
+                new_data[i] = thread_0->data[thread_0_index];
+                thread_0_index++;
+            } else {
+                new_data[i] = thread_1->data[thread_1_index];
+                thread_1_index++;
+            }
+            i++;
         }
-        else {
-            thread_0->new_data[i] = thread_0->data[data_index];
-            data_index++;
+        while (thread_0_index < thread_0->split_index && i < new_data_n) {
+            new_data[i] = thread_0->data[thread_0_index];
+            thread_0_index++;
+            i++;
+        }
+        while (thread_1_index < thread_1->split_index && i < new_data_n) {
+            new_data[i] = thread_1->data[thread_1_index];
+            thread_1_index++;
+            i++;
+        }
+    } else {
+        thread_0_index = thread_0->split_index;
+        thread_1_index = thread_1->split_index;
+        new_data_n = thread_0->data_n - thread_0->split_index + thread_1->data_n - thread_1->split_index;
+        new_data = (int *)malloc(sizeof(int) * new_data_n);
+        while (thread_0_index < thread_0->data_n &&
+               thread_1_index < thread_1->data_n) {
+            if (thread_0->data[thread_0_index] <
+                thread_1->data[thread_1_index]) {
+                new_data[i] = thread_0->data[thread_0_index];
+                thread_0_index++;
+            } else {
+                new_data[i] = thread_1->data[thread_1_index];
+                thread_1_index++;
+            }
+            i++;
+        }
+        while (thread_0_index < thread_0->data_n && i < new_data_n) {
+            new_data[i] = thread_0->data[thread_0_index];
+            thread_0_index++;
+            i++;
+        }
+        while (thread_1_index < thread_1->data_n && i < new_data_n) {
+            new_data[i] = thread_1->data[thread_1_index];
+            thread_1_index++;
+            i++;
         }
     }
-    #pragma omp barrier
-    free(thread_0->data_free);
-    thread_0->data = thread_0->new_data;
-    thread_0->data_free = thread_0->new_data;
-    thread_0->data_n = new_data_n;
-    thread_1->merge_n = 0;
+    thread_0->new_data_n = new_data_n;
+    return new_data;
 }
 
+//find split index with binary search, return index
+int find_split(int * data, int data_n, int median) {
+    int start = 0;
+    int end = data_n - 1;
+    int mid = (start + end) / 2;
+    while (start <= end) {
+        mid = (start + end) / 2;
+        if (data[mid] == median) {
+            return mid;
+        } else if (data[mid] < median) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return start;
+}
 
 void global_sort(thread_data_t **thread_data, int group_size,
                  int current_thread_id) {
@@ -100,7 +154,6 @@ void global_sort(thread_data_t **thread_data, int group_size,
     int group_start = group_id * group_size;
     int thread_id_in_group = current_thread_id % group_size;
     int median_avg = 0;
-#pragma omp barrier
     // Find average of medians of the threads belonging to the group
     // This is calculated by all threads in the group which is unnecessary, but
     // faster than the overhead of communicating the median_avg to all threads
@@ -114,57 +167,32 @@ void global_sort(thread_data_t **thread_data, int group_size,
     const int tid0 = current_thread_id;
     const int tid1 = group_start + (group_size - 1) - thread_id_in_group;
     thread_data_t *thread0 = thread_data[tid0];
-#pragma omp barrier
-    int i;
-    int move_from_current_n = 0;
-    if (thread_id_in_group < group_size / 2) {
-         for (int i = 0; i < thread0->data_n; ++i) {
-            if (thread0->data[i] >= median_avg) {
-                thread0->tmp_data[move_from_current_n] = thread0->data[i];
-                move_from_current_n++;
-            }
-        }
-        thread0->merge_n = move_from_current_n;
-        thread0->data_n -= move_from_current_n;
-    } else {
-                // Walk forward through the array and find values that are smaller than
-        // the median
-        for (int i = 0; i < thread0->data_n; i++) {
-            if (thread0->data[i] > median_avg) {
-                break;
-            }
-            thread0->tmp_data[move_from_current_n] = thread0->data[i];
-            move_from_current_n++;
-        }
-        thread0->merge_n = move_from_current_n;
-        thread0->data = thread0->data + move_from_current_n;
-        thread0->data_n -= move_from_current_n;
-    }
     thread_data_t *thread1 = thread_data[tid1];
+    int move_from_current_n = 0;
+    int split_index = find_split(thread0->data, thread0->data_n, median_avg);
+    thread0->split_index = split_index;
 #pragma omp barrier
-    move_values(thread0, thread1);
-
+    int * new_data = merge_data(thread0, thread1, (tid0 < tid1));
+#pragma omp barrier
+    free(thread0->data);
+    thread0->data = new_data;
+    thread0->data_n = thread0->new_data_n;
 #pragma omp barrier
     global_sort(thread_data, group_size / 2, current_thread_id);
 }
 
 int quick_sort_parallel(thread_data_t **thread_data, int threads_n) {
+#pragma omp parallel for
     for (int i = 0; i < threads_n; i++) {
-#pragma omp single nowait
-        {
             qsort(thread_data[i]->data, thread_data[i]->data_n, sizeof(int),
                   cmpfunc);
-        }
     }
 
-            print_array(thread_data, threads_n);
 #pragma omp parallel
     {
         for (int i = 0; i < threads_n; i++) {
 #pragma omp single nowait
             {
-                qsort(thread_data[i]->data, thread_data[i]->data_n, sizeof(int),
-                      cmpfunc);
                 int tid = omp_get_thread_num();
                 global_sort(thread_data, threads_n, tid);
             }
@@ -200,7 +228,7 @@ void setup_threads() {}
 int main() {
 
     // Actual system threads to use
-    int num_omp_threads = 4;
+    int num_omp_threads = 8;
     int n;
     //"Threads" to use for sorting (aka processors)
     int num_threads = num_omp_threads;
@@ -209,7 +237,7 @@ int main() {
     // scanf("%d", &n);
     // printf("Enter the number of threads: ");
     // scanf("%d", &threads);
-    n = 2;
+    n = 4;
     n *= 10;
     int data_n_per_thread = n / num_threads;
     // Allocate a bit more memory than needed to avoid reallocating
@@ -225,11 +253,9 @@ int main() {
         thread_data[tid]->data =
             (int *)malloc(data_n_per_thread * alloc_multiplier * sizeof(int));
         thread_data[tid]->data_free = thread_data[tid]->data;
-        thread_data[tid]->tmp_data = (int *)malloc(
-            data_n_per_thread * alloc_multiplier_tmp * sizeof(int));
         thread_data[tid]->data_n = data_n_per_thread;
         for (int i = 0; i < data_n_per_thread; i++) {
-            int rand_val = rand() % 900 + 100;
+            int rand_val = rand() % 90000 + 100;
             thread_data[tid]->data[i] = rand_val;
             builtin_qsort_data[tid * data_n_per_thread + i] = rand_val;
         }
